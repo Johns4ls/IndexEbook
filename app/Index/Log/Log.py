c188014ef98733ad94193bb4e739d23f37e5d875
Johns4ls <53236823+Johns4ls@users.noreply.github.com>
2020-06-21 17:44:56 -0400

    Added log and multiprocessing for index
    Added log for when we begin indexing files
    Added recursively search folders for ebooks
    Added multiprocessing to create 6 processes for indexing

---
+++ app/Index/Log/Log.py
@@ -0,0 +1,9 @@
+    #Set our log path
+class Tee(object):
+	def __init__(self, *targets):
+		self.targets = targets
+
+	def write(self, obj):
+		for ftarg in self.targets:
+			ftarg.write(obj)
+			ftarg.flush()  # If you want the output to be visible immediately
--- app/Index/index.py
+++ app/Index/index.py
@@ -3,14 +3,52 @@ from app.Index import dataLoader
 from app.Index.ISBNData import GoogleISBNData
 from app.Index.EbookData import EbookQuery, MobiQuery, PDFQuery
 from app.Database import database, tables
+from app.Index.Log import Tee
+from multiprocessing import Pool
 
-def index():
-    #Creates our database connection, along with creating/updating tables
-    conn = database.create_connection()
+# List files in the queue in the log
+def list_targets(media_path, file_types):
+	queue_Count = 0
+	queue_list = ''
+	check_path = os.path.exists(media_path)
+	if not check_path:
+		print "Path not found: " + media_path
+		print "Ensure your media_path exists and is accessible."
+		print "Aborting index."
+	else:
+		for root, dirs, targets in os.walk(media_path):
+			for target_name in targets:
+				if target_name.endswith(file_types):
+					queue_Count += 1
+					fullpath = os.path.normpath(os.path.join(str(root), str(target_name)))
+					queue_list = queue_list + "\n" + (str(queue_Count) + ': ' + fullpath)
+		if queue_Count == 1:
+			print ("There is " + str(queue_Count) + " file in the queue:")
+		elif queue_Count > 1:
+			print ("There are " + str(queue_Count) + " files in the queue:")
+		else:
+			print ("There are no files to be indexed in " + media_path + ".")
+		print queue_list
+        return queue_Count, queue_list
 
-    #This loads our data into the database.
+def divideList(seq):
+    return (seq[i::6] for i in range(6))
+
+def indexBooks(queue_lists):
+        try:
+        print("Spinning up processes")
+        with Pool(6) as p:
+        p.map(processBooks, queue_lists)
+    except:
+        print("Index Failed!")
+
+def processBooks(queue_list):
+    #TODO
     dataLoader.LoadFakeData(conn)
-
+    """
+    conn = database.create_connection()
+    #This loads fake data into the database.
+    
     #Get data from epub, check metadata against google books database, and then submit it to our database
     dataLoader.LoadData(conn, GoogleISBNData.query(EbookQuery.readEpub())[0])
 
@@ -19,3 +57,27 @@ def index():
 
     #Get data from PDF, check metadata against google books database, and then submit it to our database
     dataLoader.LoadData(conn, GoogleISBNData.query(PDFQuery.readPDF())[0])
+    """
+
+def index():
+    print("Starting index at " + datetime.datetime.now().strftime('%H:%M:%S') + "\n")
+
+    #Instantiate the files we can currently pull metadata from
+    file_types = '.epub', '.mobi', '.pdf'
+    #Collect the path of the file we are currently in
+    Base_Path = os.path.dirname(os.path.abspath(__file__))
+    #Path to eBook files
+    media_path = "B:/Ebooks and Audiobooks/Ebooks"
+    #Path to Log
+    log_path = Base_Path + "/Log/Index.log"
+
+    #Set up our log logic
+    ftarg = open(log_path, 'w')
+    original = sys.stdout
+    sys.stdout = Tee(sys.stdout, ftarg)
+
+    queue_count, queue_list = list_targets(media_path, file_types)
+
+    queue_lists = divideList(queue_list)
+
+    indexBooks(queue_lists)
