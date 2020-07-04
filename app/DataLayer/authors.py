3e7ed5f020fdc1812ebd9a23c85a9798e39c4268
Johns4ls <53236823+Johns4ls@users.noreply.github.com>
2020-07-04 15:54:59 -0400

    Missed file
    Missed file of authors update

--- app/DataLayer/authors.py
+++ app/DataLayer/authors.py
@@ -6,16 +6,21 @@ def insert(self, conn):
     query = "insert into authors (name) "
 
     #I used some string interpolation along with our session variable self to set up our data.
-    query += "values ('%s');" % (self.name)
+    query += """values ("%s");""" % (self.name)
     
     #Lets execute our query and set the data in the database.
-    conn.execute(query)
+    try:
+        conn.execute(query)
+    except Exception as e:
+        print(query)
+        print(e)
 
     #This has to be run to save the data.
     conn.commit()
 
 def update(self, conn):
     #TODO
+    Print("Unimplemented")
     
 def deleteFromKey(conn, authorID):
 
@@ -43,9 +48,19 @@ def findAll(conn):
     cur.execute("Select * from authors;")
     return cur.fetchall()
 
-def search(conn, info):
+def search(self, conn):
 
     #Here we are setting up a string representation of the SQL.
     cur = conn.cursor()
-    cur.execute(f"Select * from Authors WHERE name LIKE '{info}'';")
-    return cur.fetchall()
+    cur.execute(f"""Select * from Authors WHERE name LIKE "{self.name}";""")
+    return cur.fetchone()
+
+def exists(self, conn):
+    isSame = False
+    #Here we are setting up a string representation of the SQL.
+    cur = conn.cursor()
+    cur.execute(f"""Select * from Authors WHERE name LIKE "{self.name}";""")
+    results = cur.fetchall()
+    if(len(results) > 0):
+        isSame = True
+    return isSame
